6501609464a071fbe02f56d3432667823b99a52f
Sarah <sarahchosen@hotmail.co.uk>
2024-03-16 23:59:54 +0000

    dockerise app

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+// const nextConfig = {};
+
+// export default nextConfig;
+module.exports = {
+  // ... rest of the configuration.
+  output: "standalone",
+};
